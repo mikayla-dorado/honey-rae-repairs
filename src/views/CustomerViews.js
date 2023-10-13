46776135e66105bf5551c6e71fe878816b92c747
mikayla dorado <mikayla.dorado17@gmail.com>
2023-10-13 09:54:28 -0500

    created tickets

--- src/views/CustomerViews.js
+++ src/views/CustomerViews.js
@@ -2,6 +2,7 @@ import { Outlet, Route, Routes } from "react-router-dom"
 import { Welcome } from "../components/welcome/Welcome"
 import { CustomerNav } from "../components/nav/CustomerNav"
 import { TicketList } from "../components/tickets/TicketList"
+import { TicketForm } from "../components/forms/TicketForm"
 
 export const CustomerViews = ({ currentUser }) => {
 
@@ -19,7 +20,10 @@ export const CustomerViews = ({ currentUser }) => {
                 <Route index element={<Welcome />} />
                 <Route
                     path="tickets"
-                    element={<TicketList currentUser={currentUser} />} />
+                    >
+                        <Route index  element={<TicketList currentUser={currentUser} />} />
+                    <Route path="create" element={<TicketForm currentUser={currentUser} />} />
+                </Route>
             </Route>
         </Routes>
     )
